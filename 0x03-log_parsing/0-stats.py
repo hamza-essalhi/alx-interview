e14c8b22530ae95baaafafc674f388b4d8eacd02
hamza-essalhi <hamzaessalhix@gmail.com>
2024-01-25 13:17:31 -0800

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
-import signal
 
-# Dictionary to store the total file size and count of each status code
-file_size_total = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def print_statistics():
-    print(f"File size: {file_size_total}")
-    for code in sorted(status_code_count):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
-    print()
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-# Register the signal handler for interrupt (Ctrl+C)
-signal.signal(signal.SIGINT, lambda signal, frame: print_statistics())
+
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
-    line_count = 0
     for line in sys.stdin:
-        line_count += 1
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
+
+        stlist = line.split()
+        count += 1
+
         try:
-            parts = line.split()
-            ip, date, method, path, version, status_code, file_size = parts[0], parts[3][1:], parts[5][1:], parts[6], parts[8], int(parts[10]), int(parts[11])
-            if method == "GET" and path == "/projects/260" and version == "HTTP/1.1":
-                file_size_total += file_size
-                status_code_count[status_code] += 1
-
-                if line_count % 10 == 0:
-                    print_statistics()
-
-        except (ValueError, IndexError):
-            # Skip lines with incorrect format
+            size += int(stlist[-1])
+        except Exception:
             pass
 
-except KeyboardInterrupt:
-    # Handle Ctrl+C interrupt and print final statistics
-    print_statistics()
-    sys.exit(0)
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
 
+
+except KeyboardInterrupt:
+    printStatus(statusCodes, size)
+    raise
